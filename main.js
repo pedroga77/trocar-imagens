3e19b0f407a91a5eba47d8652382bd8763961b10
BRIAN BARBOSA MARTINS <24122275@CFP127ALUNO.LOCAL>
2025-01-28 12:25:14 -0300

    ok

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const botaoTrocarImagem = document.getElementById('trocar-imagem')
+
+const
+
+function trocarImagem () {
+    document.documentElement.style.setProperty('--fundo-imagem')
+}
+
+botaoTrocarcor.addEventListener('click', trocarImagem)
